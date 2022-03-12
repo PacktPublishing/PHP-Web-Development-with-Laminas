de5e5f08eb2b2d0e6d7a635def6df46457a626a2
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-03-12 11:34:25 -0300

    Place first snapshot of whatstore into folder chapter05 according to Mark's guideline #11

---
+++ chapter05/whatstore/config/modules.config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\ZendFrameworkBridge',
+    'Store',
+    'Inventory',
+    'Application',
+];
