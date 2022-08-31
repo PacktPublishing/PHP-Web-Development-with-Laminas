762cbe5eb23ee7a87ba56fd175bd8aa4e8cfcccd
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-08-31 20:54:21 -0300

    Update laminas-mvc and other necessary dependencies #45

--- chapter08/whatstore/config/modules.config.php
+++ chapter08/whatstore/config/modules.config.php
@@ -6,10 +6,14 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\I18n',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
     'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
-    'Laminas\ZendFrameworkBridge',
     'Generic',
     'Store',
     'Inventory',
